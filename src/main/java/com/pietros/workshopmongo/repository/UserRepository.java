3b118e4d2ca6af730305bb31428ed12b1dfac300
Pietros Balbino <pietros.balbino@gmail.com>
2021-07-30 18:11:50 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/pietros/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pietros.workshopmongo.repository;
+
+import com.pietros.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}

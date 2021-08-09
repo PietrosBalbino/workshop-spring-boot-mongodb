b4cfefb5c74f82758b000edd8b8024c8b5cbfa6a
Pietros Balbino <pietros.balbino@gmail.com>
2021-08-09 16:39:27 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/pietros/workshopmongo/resource/UserResource.java
+++ src/main/java/com/pietros/workshopmongo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.pietros.workshopmongo.resource;
 
+import com.pietros.workshopmongo.domain.Post;
 import com.pietros.workshopmongo.domain.User;
 import com.pietros.workshopmongo.dto.UserDTO;
 import com.pietros.workshopmongo.services.UserService;
@@ -59,5 +60,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
